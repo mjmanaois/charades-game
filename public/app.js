9a4ca38f977007dde5e330c26fda0b9d7aee605b
mjmanaois <mjmanaois2024@gmail.com>
2026-05-31 19:18:10 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,39 +1,39 @@
-const socket = io();
+// const socket = io();
 
-let role = "";
+// let role = "";
 
-socket.emit("joinGame");
+// socket.emit("joinGame");
 
-function startGame() {
-  socket.emit("playerReady");
-}
+// function startGame() {
+//   socket.emit("playerReady");
+// }
 
-socket.on("playerCount", (count) => {
-  console.log("Players:", count);
-});
+// socket.on("playerCount", (count) => {
+//   console.log("Players:", count);
+// });
 
-socket.on("role", (data) => {
-  role = data.role;
+// socket.on("role", (data) => {
+//   role = data.role;
 
-  if (role === "drawer") {
-    document.getElementById("status").innerText =
-      "You are DRAWING: " + data.word;
-  }
+//   if (role === "drawer") {
+//     document.getElementById("status").innerText =
+//       "You are DRAWING: " + data.word;
+//   }
 
-  if (role === "guesser") {
-    document.getElementById("status").innerText =
-      "You are GUESSING";
-  }
-});
+//   if (role === "guesser") {
+//     document.getElementById("status").innerText =
+//       "You are GUESSING";
+//   }
+// });
 
-socket.on("gameMessage", (msg) => {
-  document.getElementById("message").innerText = msg;
-});
+// socket.on("gameMessage", (msg) => {
+//   document.getElementById("message").innerText = msg;
+// });
 
-/**
- * GUESS INPUT
- */
-function sendGuess() {
-  const guess = document.getElementById("guessInput").value;
-  socket.emit("guess", guess);
-}
+// /**
+//  * GUESS INPUT
+//  */
+// function sendGuess() {
+//   const guess = document.getElementById("guessInput").value;
+//   socket.emit("guess", guess);
+// }
