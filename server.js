882416ec3f43fed70542ed1854058a041682852a
mjmanaois <mjmanaois2024@gmail.com>
2026-05-31 17:35:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -146,6 +146,8 @@ function startRound(room) {
   );
 }
 
-server.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
